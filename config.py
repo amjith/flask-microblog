2dec4476110d5c55c2cd4203bcc5cc3796e154fb
Amjith Ramanujam <amjith.r@gmail.com>
2013-11-11 05:27:21 -0800

    Implement user login system.

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-#import os
-#basedir = os.path.abspath(os.path.dirname(__file__))
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-#SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'migrations')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'migrations')
 CSRF_ENABLED = True
 SECRET_KEY = 'ThisIsAReallyLongKey-ThatYoullNeverGuess'
 
