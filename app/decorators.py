48cd79370ea46135501e08b9969626b4dee32ab9
Amjith Ramanujam <amjith.r@gmail.com>
2013-11-29 06:40:30 -0800

    Add bootstrap changes to code.

---
+++ app/decorators.py
@@ -0,0 +1,9 @@
+from threading import Thread
+import functools
+
+def async(f):
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        thr = Thread(target=f, args=args, kwargs=kwargs)
+        thr.start()
+    return wrapper
